8beab04ec96011100d6a9dfec109a810dfe37829
Christian Tönnes <christian.toennes@medma.uni-heidelberg.de>
2020-11-06 15:03:55 +0100

    updated  i0 parameter calculation

--- i0.py
+++ i0.py
@@ -60,7 +60,7 @@ def plot_luts(luts):
 def parse_stpar(data):
     sh_stpar_data_format = '17h6c9h2c7h6c7h4ch4c4h4c17h51i54fh6c'
 
-def calc_i0(s):
+def norm_images(s):
     im_norm = s["im"]
     print("invert gamma luts")
     igamma = np.array([inverse_lut(gamma) for gamma in s["gamma"]])
@@ -69,18 +69,60 @@ def calc_i0(s):
     print("averaging")
     im_norm = np.array([np.mean(i) for i in im_norm])
     print("reverse windowing")
-    im_norm = im_norm*(s["ww"]/4096) + (s["wc"]-s["ww"]/2)
+    im_norm_windowed = im_norm*(s["ww"]/4096) + (s["wc"]-s["ww"]/2)
+    im_windowed = s["im"]*(s["ww"]/4096) + (s["wc"]-s["ww"]/2)
     s["im_norm"] = im_norm
+    s["im_norm_windowed"] = im_norm_windowed
+    s["im_windowed"] = im_windowed
     return s
 
 def fit_i0(s):
-    a, b = np.polyfit(s["μAs"]*0.001, s["im_norm"], 1)
-    a1, b1 = np.polyfit(s["mA"]*s["pw"]*0.001, s["im_norm"], 1)
+    print("fit functions")
+    f1 = np.polyfit(s["μAs"]*0.001, s["im_norm"], 2)
+    f2 = np.polyfit(s["μAs"]*0.001, s["im_norm_windowed"], 2)
     avg = np.array([np.mean(i) for i in s["im"]])
-    a2, b2 = np.polyfit(s["μAs"]*0.001, avg,  1)
-    a3, b3 = np.polyfit(s["mA"]*s["pw"]*0.001, avg, 1)
-    return (a,b), (a1,b1), (a2,b2), (a3,b3)
+    avg_windowed = np.array([np.mean(i) for i in s["im_windowed"]])
+    f3 = np.polyfit(s["μAs"]*0.001, avg_windowed,  2)
+    f4 = np.polyfit(s["μAs"]*0.001, avg, 2)
+    return f1, f2, f3, f4
+
+def get_i0(input_path = r"D:\lumbal_spine_13.10.2020\output\70kVp"):
+    s = read_dicoms(input_path)
+    avg = np.array([np.mean(i[:,100:-100,100:-100]) for i in s["im"]])
+    image_count = np.array([len(i) for i in s["im"]])
+    f = np.polyfit(s["μAs"]*0.001/image_count, avg, 2)
+    #f = np.polyfit(s["mA"]*s["pw"]*0.001, avg, 2)
+    return f, s["gamma"][0]
+
 
 if __name__ == "__main__":
     s = read_dicoms(r"D:\lumbal_spine_13.10.2020\output\70kVp")
     
+    s = norm_images(s)
+    f1, f2, f3, f4 = fit_i0(s)
+    import matplotlib.pyplot as plt
+    x = np.linspace(np.min(s["μAs"]*0.001), np.max(s["μAs"]*0.001), len(s["μAs"]))
+    y = s["μAs"]*0.001
+    ax = plt.subplot(222)
+    plt.title("gamma correction")
+    plt.plot(x, np.polyval(f1, x), "r", label="{}".format(f1))
+    plt.scatter(y, s["im_norm"], c="g", label="")
+    plt.legend()
+    ax = plt.subplot(224)
+    plt.title("gamma correction + windowing reversed")
+    plt.plot(x, np.polyval(f2, x), "r", label="{}".format(f2))
+    plt.scatter(y, s["im_norm_windowed"], c="g", label="")
+    plt.legend()
+    ax = plt.subplot(223)
+    plt.title("windowing reversed")
+    plt.plot(x, np.polyval(f3, x), "r", label="{}".format(f3))
+    avg = np.array([np.mean(i) for i in s["im_windowed"]])
+    plt.scatter(y, avg, c="g", label="")
+    plt.legend()
+    ax = plt.subplot(221)
+    plt.title("no corrections")
+    plt.plot(x, np.polyval(f4, x), "r", label="{}".format(f4))
+    avg = np.array([np.mean(i) for i in s["im"]])
+    plt.scatter(y, avg, c="g", label="")
+    plt.legend()
+    plt.show()
