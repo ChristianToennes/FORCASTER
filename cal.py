corrected gradient calculation

cal.py
@@ -225,9 +225,8 @@ gi_skip = 1
 gi_mask = None
 gi_shape = None
 def GI(new_img, p1, absp1):
-    p2 = np.array(np.meshgrid(new_img[1::gi_skip,::gi_skip]-new_img[:-1:gi_skip,::gi_skip], new_img[::gi_skip,1::gi_skip]-new_img[::gi_skip,:-1:gi_skip], copy=False)).reshape(2,-1)
+    p2 = new_img[1::gi_skip,::gi_skip]-new_img[:-1:gi_skip,::gi_skip], new_img[::gi_skip,1::gi_skip]-new_img[::gi_skip,:-1:gi_skip]
     absp2 = np.sqrt(p2[0]*p2[0] + p2[1]*p2[1], dtype=np.float16)
-    #absp2 = np.linalg.norm(p2, axis=0)
     absGrad = absp1*absp2
     minabs = np.min(np.array([absp1, absp2]), axis=0)
     del absp2
@@ -242,7 +241,7 @@ def GI(new_img, p1, absp1):
     del absGrad
     r = w*minabs
     del w
-    ret = np.sum(r, dtype=np.float32)
+    ret = np.sum(r)#, dtype=np.float32)
     del r
     return ret
 
@@ -267,8 +266,8 @@ def calcGIObjective(old_img_big, new_img_big, i, cur, config):
     global gi_mask, gi_shape, gi_skip
     if gi_mask is None:
         gi_mask = np.zeros_like(old_img_big, dtype=bool)
-        b1 = old_img_big.shape[0]//3
-        b2 = old_img_big.shape[1]//3
+        b1 = old_img_big.shape[0]//4
+        b2 = old_img_big.shape[1]//4
         gi_mask[b1:-b1,b2:-b2] = True
         gi_shape = (old_img_big.shape[0]-b1-b1, old_img_big.shape[1]-b2-b2)
 
@@ -279,11 +278,8 @@ def calcGIObjective(old_img_big, new_img_big, i, cur, config):
         if np.linalg.norm(k[0]-cur[0]) < 0.01 and np.linalg.norm(k[1]-cur[1]) < 0.01 and np.linalg.norm(k[2]-cur[2]) < 0.01:
             return gis[i][key]
 
-    p1 = np.array(np.meshgrid(old_img[1::gi_skip,::gi_skip]-old_img[:-1:gi_skip,::gi_skip], old_img[::gi_skip,1::gi_skip]-old_img[::gi_skip,:-1:gi_skip], copy=False)).reshape(2,-1)
+    p1 = old_img[1::gi_skip,::gi_skip]-old_img[:-1:gi_skip,::gi_skip], old_img[::gi_skip,1::gi_skip]-old_img[::gi_skip,:-1:gi_skip]
     if config["GIoldold"][i] is None:
-        #p1 = np.meshgrid(old_img[1::s,::s]-old_img[:-1:s,::s], old_img[::s,1::s]-old_img[::s,:-1:s], copy=False)
-        #config["p1"][i] = p1
-        #config["absp1"] = np.linalg.norm([p1[0].flatten(),p1[1].flatten()], axis=0)
         config["absp1"][i] = np.sqrt(p1[0]*p1[0] + p1[1]*p1[1],dtype=np.float16)
         config["GIoldold"][i] = GI(old_img, p1, config["absp1"][i])
     #perftime = time.perf_counter()